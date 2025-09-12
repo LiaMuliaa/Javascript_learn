e09ef265ea5bc54c3b71e814fa4944f0c0ffc7ca
MuliaSahpira <muliasahpira@gmail.com>
2025-09-12 17:08:15 +0700

    fetch

--- Advanced Javascript/Fetch/fetch.js
+++ Advanced Javascript/Fetch/fetch.js
@@ -1,9 +1,32 @@
 // Fetch
 const searchButton = document.querySelector('.search-button');
 searchButton.addEventListener('click', function() {
-    const inputKeyboard = document.querySelector(`.input-keyword`);
+
+    const inputKeyword = document.querySelector('.input-keyword');
     fetch('http://www.omdbapi.com/?apikey=a71aad37&s=' + inputKeyword.value)
-        .then(response => console.log(response));
+        .then(response => response.json())
+        .then(response => {
+            const movies = response.Search;
+            let cards = '';
+            movies.forEach(m => cards += showCards(m));
+            const movieContainer = document.querySelector('.movie-container');
+            movieContainer.innerHTML = cards;
+
+            // ketika tombol detail di-klik
+            const modalDetailButton = document.querySelectorAll('.modal-detail-button');
+            modalDetailButton.forEach(btn => {
+                btn.addEventListener('click', function () {
+                    const imdbid = this.dataset.imdbid;
+                    fetch('http://www.omdbapi.com/?apikey=a71aad37&i=' + imdbid)
+                        .then(response => response.json())
+                        .then(m => {
+                            const movieDetail = showMovieDetail(m);
+                            const modalBody = document.querySelector('.modal-body');
+                            modalBody.innerHTML = movieDetail;
+                        })
+                })
+            })
+        });
 });
 
 
